2e63f9fb330729808407b555415b96be91ca999b
letieu <letieu8@gmail.com>
2024-05-16 00:33:52 +0700

    feat: error message

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ import { bodyParser } from "@koa/bodyparser";
 const app = new Koa();
 app.use(bodyParser());
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err: any) {
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      message: err.message
+    };
+  }
+})
+
 app.use(async function (ctx) {
   const body = ctx.request.body;
   const taxIds = body.taxIds;
-
-  console.log(taxIds);
 
   if (crawler.running) {
     ctx.body = "Running before, please wait";
@@ -21,5 +30,3 @@ app.use(async function (ctx) {
 });
 
 app.listen(3000);
-
-//crawler.run([getUrl("0316624050"), getUrl("0110481627")]);
